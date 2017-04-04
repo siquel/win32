db81a06c96c0937a5a8dc4286d9d399068380a41
siquel <squual@kahvipaussi.net>
2017-04-04 19:38:59 +0300

    Add keycodes and raw input

---
+++ src/hotswap.h
@@ -0,0 +1,167 @@
+#pragma once
+
+namespace hotswap
+{
+    struct Keyboard
+    {
+        enum Key
+        {
+            Backspace,
+            Tab,
+            Enter,
+            CapsLock,
+            Escape,
+            Space,
+            PageUp,
+            PageDown,
+            End,
+            Home,
+            Left,
+            Up,
+            Right,
+            Down,
+            Select,
+            Print,
+            Execute,
+            PrintScreen,
+            Insert,
+            Delete,
+            Help,
+            D0,              ///< The 0 key
+            D1,              ///< The 1 key
+            D2,              ///< The 2 key
+            D3,              ///< The 3 key
+            D4,              ///< The 4 key
+            D5,              ///< The 5 key
+            D6,              ///< The 6 key
+            D7,              ///< The 7 key
+            D8,              ///< The 8 key
+            D9,              ///< The 9 key
+            A,               ///< The A key
+            B,               ///< The B key
+            C,               ///< The C key
+            D,               ///< The D key
+            E,               ///< The E key
+            F,               ///< The F key
+            G,               ///< The G key
+            H,               ///< The H key
+            I,               ///< The I key
+            J,               ///< The J key
+            K,               ///< The K key
+            L,               ///< The L key
+            M,               ///< The M key
+            N,               ///< The N key
+            O,               ///< The O key
+            P,               ///< The P key
+            Q,               ///< The Q key
+            R,               ///< The R key
+            S,               ///< The S key
+            T,               ///< The T key
+            U,               ///< The U key
+            V,               ///< The V key
+            W,               ///< The W key
+            X,               ///< The X key
+            Y,               ///< The Y key
+            Z,               ///< The Z key
+            LeftSystem,      ///< The left system key
+            RightSystem,     ///< The right system key
+            Applications,
+            Sleep,
+            NumPad0,         ///< The numpad 0 key
+            NumPad1,         ///< The numpad 1 key
+            NumPad2,         ///< The numpad 2 key
+            NumPad3,         ///< The numpad 3 key
+            NumPad4,         ///< The numpad 4 key
+            NumPad5,         ///< The numpad 5 key
+            NumPad6,         ///< The numpad 6 key
+            NumPad7,         ///< The numpad 7 key
+            NumPad8,         ///< The numpad 8 key
+            NumPad9,         ///< The numpad 9 key
+            Multiply,        ///< The numpad multiply key
+            Add,             ///< The numpad add key
+            NumpadEnter,     ///< The numpad enter (separator) key  
+            Subtract,        ///< The numpad subtract add key
+            Decimal,         ///< The numpad decimal add key
+            Divide,          ///< The numpad divide add key
+            F1,              ///< The F1 key
+            F2,              ///< The F2 key
+            F3,              ///< The F3 key
+            F4,              ///< The F4 key
+            F5,              ///< The F5 key
+            F6,              ///< The F6 key
+            F7,              ///< The F7 key
+            F8,              ///< The F8 key
+            F9,              ///< The F9 key
+            F10,             ///< The F10 key
+            F11,             ///< The F11 key
+            F12,             ///< The F12 key
+            F13,             ///< The F13 key
+            F14,             ///< The F14 key
+            F15,             ///< The F15 key
+            F16,             ///< The F16 key
+            F17,             ///< The F17 key
+            F18,             ///< The F18 key
+            F19,             ///< The F19 key
+            F20,             ///< The F20 key
+            F21,             ///< The F21 key
+            F22,             ///< The F22 key
+            F23,             ///< The F23 key
+            F24,             ///< The F24 key
+            NumLock,         ///< The numlock key
+            ScrollLock,      ///< The scroll lock key
+            LeftShift,       ///< The left shift key
+            RightShift,      ///< The right shift key
+            LeftControl,     ///< The left control key
+            RightControl,    ///< The left control key
+            LeftAlt,
+            RightAlt,
+            BrowserBack,
+            BrowserForward,
+            BrowserRefresh,
+            BrowserStop,
+            BrowserSearch,
+            BrowserFavorites,
+            BrowserHome,
+            VolumeMute,
+            VolumeDown,
+            VolumeUp,
+            MediaNextTrack,
+            MediaPreviousTrack,
+            MediaStop,
+            MediaPlayPause,
+            LaunchMail,
+            SelectMedia,
+            LaunchApplication1,
+            LaunchApplication2,
+            OemSemicolon,
+            OemPlus,
+            OemComma,
+            OemMinus,
+            OemPeriod,
+            OemQuestion,
+            OemTilde,
+            OemOpenBrackets,
+            OemPipe,
+            OemCloseBrackets,
+            OemQuotes,
+            Oem8,
+            OemBackslash,
+            ProcessKey,
+            Attn,
+            Crsel,
+            Exsel,
+            EraseEof,
+            Play,
+            Zoom,
+            Pa1,
+            OemClear,
+            Pause,
+            ImeConvert,
+            ImeNoConvert,
+            Kana,        ///< The Kana key on Japanese keyboards.
+            Kanji,       ///< The Kanji key on Japanese keyboards
+            Count
+        };
+    };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,18 @@
 #if HOTSWAP_PLATFORM_WIN32
 
 #include <Windows.h>
+#include <stdint.h> // uint8_t
+#include "hotswap.h"
+
+// http://cnicholson.net/2011/01/stupid-c-tricks-a-better-sizeof_array/
+template<typename T, size_t N> char(&COUNTOF_REQUIRES_ARRAY_ARGUMENT(const T(&)[N]))[N];
+#define HOTSWAP_COUNTOF(x) sizeof(COUNTOF_REQUIRES_ARRAY_ARGUMENT(x))
 
 static bool s_exit = false;
+
+#define MAX_INPUT_KEYS 256
+static uint8_t s_translateKey[MAX_INPUT_KEYS];
+
 
 LRESULT CALLBACK win32WindowProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
 {
@@ -19,10 +29,235 @@ LRESULT CALLBACK win32WindowProc(HWND window, UINT message, WPARAM wparam, LPARA
         s_exit = true;
     } break;
 
+    case WM_INPUT:
+    {
+        // Adapted from https://blog.molecular-matters.com/2011/09/05/properly-handling-keyboard-input/
+
+        char inputBuffer[sizeof(RAWINPUT)];
+        UINT inputBufferSize = sizeof(inputBuffer);
+
+        if (inputBufferSize != GetRawInputData(
+            (HRAWINPUT)lparam, RID_INPUT, inputBuffer, &inputBufferSize, sizeof(RAWINPUTHEADER)))
+        {
+            OutputDebugStringA("GetRawInputData did not return correct size\n");
+            break;
+        }
+
+        RAWINPUT* input = (RAWINPUT*)inputBuffer;
+
+        if (input->header.dwType != RIM_TYPEKEYBOARD) break;
+
+        const RAWKEYBOARD* rawKeyboard = &input->data.keyboard;
+
+        UINT vkey = rawKeyboard->VKey;
+        UINT flags = rawKeyboard->Flags;
+        
+        // Fake key
+        if (255 == vkey) 
+        {
+            break;
+        }
+        else if (vkey == VK_SHIFT) // Map VK_SHIFT to VK_LSHIFT or VK_RSHIFT
+        {
+            // MapVirtualKey maps left and right hand keys
+            // but can't map it with Control and Alt keys
+            vkey = MapVirtualKeyA(rawKeyboard->MakeCode, MAPVK_VSC_TO_VK_EX);
+        }
+
+        // Reference: https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
+        bool isE0 = (flags & RI_KEY_E0) != 0;
+        
+        switch (vkey)
+        {
+        case VK_CONTROL: vkey = (isE0) ? VK_RCONTROL : VK_LCONTROL; break;
+        case VK_MENU: vkey = (isE0) ? VK_RMENU : VK_LMENU; break;
+            // the standard INSERT, DELETE, HOME, END, PRIOR and NEXT 
+            // keys will always have their e0 bit set, but the
+            // corresponding keys on the NUMPAD will not.
+        case VK_INSERT: vkey = (!isE0) ? VK_NUMPAD0 : VK_INSERT; break;
+        case VK_DELETE: vkey = (!isE0) ? VK_DECIMAL : VK_DELETE; break;
+        case VK_HOME: vkey = (!isE0) ? VK_NUMPAD7 : VK_HOME; break;
+        case VK_END: vkey = (!isE0) ? VK_NUMPAD1 : VK_END; break;
+        case VK_PRIOR: vkey = (!isE0) ? VK_NUMPAD9 : VK_PRIOR; break;
+        case VK_NEXT: vkey = (!isE0) ? VK_NUMPAD3 : VK_NEXT; break;
+            // the standard arrow keys will always have their e0 bit set, but the
+            // corresponding keys on the NUMPAD will not.
+        case VK_LEFT: vkey = (!isE0) ? VK_NUMPAD4 : VK_LEFT; break;
+        case VK_RIGHT: vkey = (!isE0) ? VK_NUMPAD6 : VK_RIGHT; break;
+        case VK_UP: vkey = (!isE0) ? VK_NUMPAD8 : VK_UP; break;
+        case VK_DOWN: vkey = (!isE0) ? VK_NUMPAD2 : VK_DOWN; break;
+        case VK_CLEAR: vkey = (!isE0) ? VK_NUMPAD5 : VK_CLEAR; break;
+        }
+        
+    } break;
+
     default:
         return DefWindowProc(window, message, wparam, lparam);
     }
     return 0;
+}
+
+void win32InitTranslateKeys()
+{
+    using namespace hotswap;
+
+    s_translateKey[VK_BACK] = Keyboard::Key::Backspace;
+    s_translateKey[VK_TAB] = Keyboard::Key::Tab;
+    s_translateKey[VK_RETURN] = Keyboard::Key::Enter;
+    s_translateKey[VK_CAPITAL] = Keyboard::Key::CapsLock;
+    s_translateKey[VK_ESCAPE] = Keyboard::Key::Escape;
+    s_translateKey[VK_SPACE] = Keyboard::Key::Space;
+    s_translateKey[VK_PRIOR] = Keyboard::Key::PageUp;
+    s_translateKey[VK_NEXT] = Keyboard::Key::PageDown;
+    s_translateKey[VK_END] = Keyboard::Key::End;
+    s_translateKey[VK_HOME] = Keyboard::Key::Home;
+    s_translateKey[VK_LEFT] = Keyboard::Key::Left;
+    s_translateKey[VK_UP] = Keyboard::Key::Up;
+    s_translateKey[VK_RIGHT] = Keyboard::Key::Right;
+    s_translateKey[VK_DOWN] = Keyboard::Key::Down;
+    s_translateKey[VK_SELECT] = Keyboard::Key::Select;
+    s_translateKey[VK_PRINT] = Keyboard::Key::Print;
+    s_translateKey[VK_EXECUTE] = Keyboard::Key::Execute;
+    s_translateKey[VK_SNAPSHOT] = Keyboard::Key::PrintScreen;
+    s_translateKey[VK_INSERT] = Keyboard::Key::Insert;
+    s_translateKey[VK_DELETE] = Keyboard::Key::Delete;
+    s_translateKey[VK_HELP] = Keyboard::Key::Help;
+
+    s_translateKey[uint8_t('0')] = Keyboard::Key::D0;
+    s_translateKey[uint8_t('1')] = Keyboard::Key::D1;
+    s_translateKey[uint8_t('2')] = Keyboard::Key::D2;
+    s_translateKey[uint8_t('3')] = Keyboard::Key::D3;
+    s_translateKey[uint8_t('4')] = Keyboard::Key::D4;
+    s_translateKey[uint8_t('5')] = Keyboard::Key::D5;
+    s_translateKey[uint8_t('6')] = Keyboard::Key::D6;
+    s_translateKey[uint8_t('7')] = Keyboard::Key::D7;
+    s_translateKey[uint8_t('8')] = Keyboard::Key::D8;
+    s_translateKey[uint8_t('9')] = Keyboard::Key::D9;
+
+    s_translateKey[uint8_t('A')] = Keyboard::Key::A;
+    s_translateKey[uint8_t('B')] = Keyboard::Key::B;
+    s_translateKey[uint8_t('C')] = Keyboard::Key::C;
+    s_translateKey[uint8_t('D')] = Keyboard::Key::D;
+    s_translateKey[uint8_t('E')] = Keyboard::Key::E;
+    s_translateKey[uint8_t('F')] = Keyboard::Key::F;
+    s_translateKey[uint8_t('G')] = Keyboard::Key::G;
+    s_translateKey[uint8_t('H')] = Keyboard::Key::H;
+    s_translateKey[uint8_t('I')] = Keyboard::Key::I;
+    s_translateKey[uint8_t('J')] = Keyboard::Key::J;
+    s_translateKey[uint8_t('K')] = Keyboard::Key::K;
+    s_translateKey[uint8_t('L')] = Keyboard::Key::L;
+    s_translateKey[uint8_t('M')] = Keyboard::Key::M;
+    s_translateKey[uint8_t('N')] = Keyboard::Key::N;
+    s_translateKey[uint8_t('O')] = Keyboard::Key::O;
+    s_translateKey[uint8_t('P')] = Keyboard::Key::P;
+    s_translateKey[uint8_t('Q')] = Keyboard::Key::Q;
+    s_translateKey[uint8_t('R')] = Keyboard::Key::R;
+    s_translateKey[uint8_t('S')] = Keyboard::Key::S;
+    s_translateKey[uint8_t('T')] = Keyboard::Key::T;
+    s_translateKey[uint8_t('U')] = Keyboard::Key::U;
+    s_translateKey[uint8_t('V')] = Keyboard::Key::V;
+    s_translateKey[uint8_t('W')] = Keyboard::Key::W;
+    s_translateKey[uint8_t('X')] = Keyboard::Key::X;
+    s_translateKey[uint8_t('Y')] = Keyboard::Key::Y;
+    s_translateKey[uint8_t('Z')] = Keyboard::Key::Z;
+
+    s_translateKey[VK_LWIN] = Keyboard::Key::LeftSystem;
+    s_translateKey[VK_RWIN] = Keyboard::Key::RightSystem;
+    s_translateKey[VK_APPS] = Keyboard::Key::Applications;
+    s_translateKey[VK_SLEEP] = Keyboard::Key::Sleep;
+    s_translateKey[VK_NUMPAD0] = Keyboard::Key::NumPad0;
+    s_translateKey[VK_NUMPAD1] = Keyboard::Key::NumPad1;
+    s_translateKey[VK_NUMPAD2] = Keyboard::Key::NumPad2;
+    s_translateKey[VK_NUMPAD3] = Keyboard::Key::NumPad3;
+    s_translateKey[VK_NUMPAD4] = Keyboard::Key::NumPad4;
+    s_translateKey[VK_NUMPAD5] = Keyboard::Key::NumPad5;
+    s_translateKey[VK_NUMPAD6] = Keyboard::Key::NumPad6;
+    s_translateKey[VK_NUMPAD7] = Keyboard::Key::NumPad7;
+    s_translateKey[VK_NUMPAD8] = Keyboard::Key::NumPad8;
+    s_translateKey[VK_NUMPAD9] = Keyboard::Key::NumPad9;
+    s_translateKey[VK_MULTIPLY] = Keyboard::Key::Multiply;
+    s_translateKey[VK_ADD] = Keyboard::Key::Add;
+    s_translateKey[VK_SEPARATOR] = Keyboard::Key::NumpadEnter;
+    s_translateKey[VK_SUBTRACT] = Keyboard::Key::Subtract;
+    s_translateKey[VK_DECIMAL] = Keyboard::Key::Decimal;
+    s_translateKey[VK_DIVIDE] = Keyboard::Key::Divide;
+    s_translateKey[VK_F1] = Keyboard::Key::F1;
+    s_translateKey[VK_F2] = Keyboard::Key::F2;
+    s_translateKey[VK_F3] = Keyboard::Key::F3;
+    s_translateKey[VK_F4] = Keyboard::Key::F4;
+    s_translateKey[VK_F5] = Keyboard::Key::F5;
+    s_translateKey[VK_F6] = Keyboard::Key::F6;
+    s_translateKey[VK_F7] = Keyboard::Key::F7;
+    s_translateKey[VK_F8] = Keyboard::Key::F8;
+    s_translateKey[VK_F9] = Keyboard::Key::F9;
+    s_translateKey[VK_F10] = Keyboard::Key::F10;
+    s_translateKey[VK_F11] = Keyboard::Key::F11;
+    s_translateKey[VK_F12] = Keyboard::Key::F12;
+    s_translateKey[VK_F13] = Keyboard::Key::F13;
+    s_translateKey[VK_F14] = Keyboard::Key::F14;
+    s_translateKey[VK_F15] = Keyboard::Key::F15;
+    s_translateKey[VK_F16] = Keyboard::Key::F16;
+    s_translateKey[VK_F17] = Keyboard::Key::F17;
+    s_translateKey[VK_F18] = Keyboard::Key::F18;
+    s_translateKey[VK_F19] = Keyboard::Key::F19;
+    s_translateKey[VK_F20] = Keyboard::Key::F20;
+    s_translateKey[VK_F21] = Keyboard::Key::F21;
+    s_translateKey[VK_F22] = Keyboard::Key::F22;
+    s_translateKey[VK_F23] = Keyboard::Key::F23;
+    s_translateKey[VK_F24] = Keyboard::Key::F24;
+    s_translateKey[VK_NUMLOCK] = Keyboard::Key::NumLock;
+    s_translateKey[VK_SCROLL] = Keyboard::Key::ScrollLock;
+    s_translateKey[VK_LSHIFT] = Keyboard::Key::LeftShift;
+    s_translateKey[VK_RSHIFT] = Keyboard::Key::RightShift;
+    s_translateKey[VK_LCONTROL] = Keyboard::Key::LeftControl;
+    s_translateKey[VK_RCONTROL] = Keyboard::Key::RightControl;
+    s_translateKey[VK_LMENU] = Keyboard::Key::LeftAlt;
+    s_translateKey[VK_RMENU] = Keyboard::Key::RightAlt;
+    s_translateKey[VK_BROWSER_BACK] = Keyboard::Key::BrowserBack;
+    s_translateKey[VK_BROWSER_FORWARD] = Keyboard::Key::BrowserForward;
+    s_translateKey[VK_BROWSER_REFRESH] = Keyboard::Key::BrowserRefresh;
+    s_translateKey[VK_BROWSER_STOP] = Keyboard::Key::BrowserStop;
+    s_translateKey[VK_BROWSER_SEARCH] = Keyboard::Key::BrowserSearch;
+    s_translateKey[VK_BROWSER_FAVORITES] = Keyboard::Key::BrowserFavorites;
+    s_translateKey[VK_BROWSER_HOME] = Keyboard::Key::BrowserHome;
+    s_translateKey[VK_VOLUME_MUTE] = Keyboard::Key::VolumeMute;
+    s_translateKey[VK_VOLUME_DOWN] = Keyboard::Key::VolumeDown;
+    s_translateKey[VK_VOLUME_UP] = Keyboard::Key::VolumeUp;
+    s_translateKey[VK_MEDIA_NEXT_TRACK] = Keyboard::Key::MediaNextTrack;
+    s_translateKey[VK_MEDIA_PREV_TRACK] = Keyboard::Key::MediaPreviousTrack;
+    s_translateKey[VK_MEDIA_STOP] = Keyboard::Key::MediaStop;
+    s_translateKey[VK_MEDIA_PLAY_PAUSE] = Keyboard::Key::MediaPlayPause;
+    s_translateKey[VK_LAUNCH_MAIL] = Keyboard::Key::LaunchMail;
+    s_translateKey[VK_LAUNCH_MEDIA_SELECT] = Keyboard::Key::SelectMedia;
+    s_translateKey[VK_LAUNCH_APP1] = Keyboard::Key::LaunchApplication1;
+    s_translateKey[VK_LAUNCH_APP2] = Keyboard::Key::LaunchApplication2;
+    s_translateKey[VK_OEM_1] = Keyboard::Key::OemSemicolon;
+    s_translateKey[VK_OEM_PLUS] = Keyboard::Key::OemPlus;
+    s_translateKey[VK_OEM_COMMA] = Keyboard::Key::OemComma;
+    s_translateKey[VK_OEM_MINUS] = Keyboard::Key::OemMinus;
+    s_translateKey[VK_OEM_PERIOD] = Keyboard::Key::OemPeriod;
+    s_translateKey[VK_OEM_2] = Keyboard::Key::OemQuestion;
+    s_translateKey[VK_OEM_3] = Keyboard::Key::OemTilde;
+    s_translateKey[VK_OEM_4] = Keyboard::Key::OemOpenBrackets;
+    s_translateKey[VK_OEM_5] = Keyboard::Key::OemPipe;
+    s_translateKey[VK_OEM_6] = Keyboard::Key::OemCloseBrackets;
+    s_translateKey[VK_OEM_7] = Keyboard::Key::OemQuotes;
+    s_translateKey[VK_OEM_8] = Keyboard::Key::Oem8;
+    s_translateKey[VK_OEM_102] = Keyboard::Key::OemBackslash;
+    s_translateKey[VK_PROCESSKEY] = Keyboard::Key::ProcessKey;
+    s_translateKey[VK_ATTN] = Keyboard::Key::Attn;
+    s_translateKey[VK_CRSEL] = Keyboard::Key::Crsel;
+    s_translateKey[VK_EXSEL] = Keyboard::Key::Exsel;
+    s_translateKey[VK_EREOF] = Keyboard::Key::EraseEof;
+    s_translateKey[VK_PLAY] = Keyboard::Key::Play;
+    s_translateKey[VK_ZOOM] = Keyboard::Key::Zoom;
+    s_translateKey[VK_PA1] = Keyboard::Key::Pa1;
+    s_translateKey[VK_OEM_CLEAR] = Keyboard::Key::OemClear;
+    s_translateKey[VK_PAUSE] = Keyboard::Key::Pause;
+    s_translateKey[VK_CONVERT] = Keyboard::Key::ImeConvert;
+    s_translateKey[VK_NONCONVERT] = Keyboard::Key::ImeNoConvert;
+    s_translateKey[VK_KANA] = Keyboard::Key::Kana;
+    s_translateKey[VK_KANJI] = Keyboard::Key::Kanji;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -42,10 +277,31 @@ int WINAPI WinMain(HINSTANCE hInstance,
         return 1;
     }
 
-    int width = 1280;
-    int height = 720;
+    // Only keyboard for now
+    RAWINPUTDEVICE inputDevices[1];
+    inputDevices[0].usUsagePage = 0x01;       // To get the keyboard TLC, set UsagePage = 1 
+    inputDevices[0].usUsage = 0x06;           // ... and Usage = 6
+    inputDevices[0].dwFlags = RIDEV_NOLEGACY; // No WM_KEYDOWN messages
+    inputDevices[0].hwndTarget = 0;           // TODO: Do we need this?
 
-    HWND window = CreateWindowA("win32_window", "Win32 Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, hInstance, 0);
+    if (FALSE == RegisterRawInputDevices(inputDevices, HOTSWAP_COUNTOF(inputDevices), sizeof(RAWINPUTDEVICE)))
+    {
+        return 1;
+    }
+
+    int windowWidth = 1280;
+    int windowHeight = 720;
+
+    HWND window = CreateWindowA(
+        "win32_window", 
+        "Hotswapping", 
+        WS_OVERLAPPEDWINDOW, 
+        CW_USEDEFAULT, CW_USEDEFAULT, 
+        windowWidth, windowHeight,
+        0, 
+        0, 
+        hInstance, 
+        0);
 
     if (0 == window)
     {
@@ -58,7 +314,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     while (!s_exit)
     {
         MSG message;
-        while (PeekMessageA(&message, NULL, 0, 0, PM_REMOVE))
+        while (0 != PeekMessageA(&message, NULL, 0, 0, PM_REMOVE))
         {
             TranslateMessage(&message);
             DispatchMessageA(&message);
