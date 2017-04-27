cb6338b7a60f44e79c891be43b6f978b5cb25a77
siquel <squual@kahvipaussi.net>
2017-04-27 20:48:44 +0300

    Let genie create two solutions for hotswapping

---
+++ hotswap/hotswap.cpp
@@ -0,0 +1,5 @@
+/*
+* Copyright(c) 2017 Jani Niemelä.
+* License: https://github.com/siquel/win32/blob/master/LICENSE
+*/
+
--- scripts/genie.lua
+++ scripts/genie.lua
@@ -3,6 +3,10 @@
 -- License: https://github.com/siquel/win32/blob/master/LICENSE
 --
 
+WIN32_DIR = path.getabsolute("..")
+local WIN32_BUILD_DIR = path.join(WIN32_DIR, ".build")
+local WIN32_THIRD_PARTY_DIR = path.join(WIN32_DIR, "3rdparty")
+
 solution "win32"
 	configurations {
 		"Debug",
@@ -21,10 +25,6 @@ solution "win32"
 		}
 
 	configuration {}
-
-WIN32_DIR = path.getabsolute("..")
-local WIN32_BUILD_DIR = path.join(WIN32_DIR, ".build")
-local WIN32_THIRD_PARTY_DIR = path.join(WIN32_DIR, "3rdparty")
 
 dofile("toolchain.lua")
 if not toolchain(WIN32_BUILD_DIR, WIN32_THIRD_PARTY_DIR) then
@@ -48,3 +48,40 @@ project "win32"
 	}
 
 	strip()
+
+-- Because VS does not want to edit files when debugging
+solution "hotswap"
+	configurations {
+		"Debug",
+		"Release"
+	}
+
+	platforms {
+		"x64"
+	}
+
+	language "C++"
+
+	configuration "Debug"
+		defines {
+			"HOTSWAP_DEBUG=1"
+		}
+
+	configuration {}
+
+dofile("toolchain.lua")
+if not toolchain(WIN32_BUILD_DIR, WIN32_THIRD_PARTY_DIR) then
+	return
+end
+
+project "hotswap"
+	kind "SharedLib"
+
+	files {
+		path.join(WIN32_DIR, "hotswap/**.cpp"),
+		path.join(WIN32_DIR, "hotswap/**.h")
+	}
+
+	includedirs {
+		path.join(WIN32_DIR, "src")
+	}
